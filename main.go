f241099c11b205fde0d6ae92df201dbb7443c168
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-06-09 11:50:15 +0530

    basic file search with file name

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	fmt.Println("Welocome to KP Search!");
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println("Need at Least 2 arguments\nex: run /path text")
+		os.Exit(0)
+	}
+	fmt.Println("Welocome to KP Search!")
+	files, err := os.ReadDir(args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	for _, file := range files {
+		//fmt.Println("Proccessing: ", file.Name())
+		if !file.IsDir() && file.Name() == args[2] {
+			fmt.Println("Found file")
+			os.Exit(0)
+		}
+		// TODO: add chanels and search deep inside directories
+		/*
+			For now we just search for file names
+			content,err := os.Open(file.Name())
+		*/
+	}
+	fmt.Println("No files found with the name")
 }
